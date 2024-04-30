ea73df720572df9e8bbaa2222941164220277049
Ratchatrin sisan <148691205+Ratchatrin@users.noreply.github.com>
2024-04-30 11:02:09 +0700

    edit show password

--- frontend-page/src/component/header/ResetPassword.tsx
+++ frontend-page/src/component/header/ResetPassword.tsx
@@ -9,6 +9,7 @@ function ResetPassword() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [newPassword, setNewPassword] = useState(String);
   const [email, setEmail] = useState(String);
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
   const updateWindowWidth = () => {
     setWindowWidth(window.innerWidth);
@@ -26,7 +27,6 @@ function ResetPassword() {
         navigate("/login");
       }
     } catch (error) {
-      alert(error.response.data.message);
       setNewPassword("");
       setEmail("");
     }
@@ -42,7 +42,7 @@ function ResetPassword() {
         ) : (
           <Nav></Nav>
         )}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col  text-center">
           <p className="text-2xl underline mb-8">Reset Password</p>
           <label className="input input-bordered flex items-center gap-2  mb-5">
             <svg
@@ -90,29 +90,83 @@ function ResetPassword() {
             value={oldPassword}
           />
         </label> */}
-          <label className="input input-bordered flex items-center gap-2  mb-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input
-              type="password"
-              className="grow"
-              placeholder="New Password"
-              onChange={(ev) => {
-                setNewPassword(ev.target.value);
-              }}
-              value={newPassword}
-            />
-          </label>
+          {!showPassword ? (
+            <>
+              <label className="input input-bordered flex items-center gap-2 mb-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="w-4 h-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="password"
+                  className="grow"
+                  placeholder="Password"
+                  onChange={(ev) => {
+                    setNewPassword(ev.target.value);
+                  }}
+                  value={newPassword}
+                />
+                <label className="swap">
+                  <input type="checkbox" />
+                  <div
+                    className="swap-on"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  >
+                    Hide
+                  </div>
+                  <div className="swap-off">Show</div>
+                </label>
+              </label>
+            </>
+          ) : (
+            <>
+              <label className="input input-bordered flex items-center gap-2 mb-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="w-4 h-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder="Password"
+                  onChange={(ev) => {
+                    setNewPassword(ev.target.value);
+                  }}
+                  value={newPassword}
+                />
+                <label className="swap">
+                  <input type="checkbox" />
+                  <div className="swap-on">Hide</div>
+                  <div
+                    className="swap-off"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  >
+                    Show
+                  </div>
+                </label>
+              </label>
+            </>
+          )}
           <button
             className="btn btn-active btn-neutral"
             onClick={resetPassword}
--- frontend-page/src/component/header/Signin.tsx
+++ frontend-page/src/component/header/Signin.tsx
@@ -10,6 +10,7 @@ function Signin() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, SetPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
   const updateWindowWidth = () => {
     setWindowWidth(window.innerWidth);
@@ -100,29 +101,83 @@ function Signin() {
               value={username}
             />
           </label>
-          <label className="input input-bordered flex items-center gap-2 mb-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input
-              type="password"
-              className="grow"
-              placeholder="Password"
-              onChange={(ev) => {
-                SetPassword(ev.target.value);
-              }}
-              value={password}
-            />
-          </label>
+          {!showPassword ? (
+            <>
+              <label className="input input-bordered flex items-center gap-2 mb-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="w-4 h-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="password"
+                  className="grow"
+                  placeholder="Password"
+                  onChange={(ev) => {
+                    SetPassword(ev.target.value);
+                  }}
+                  value={password}
+                />
+                <label className="swap">
+                  <input type="checkbox" />
+                  <div
+                    className="swap-on"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  >
+                    Hide
+                  </div>
+                  <div className="swap-off">Show</div>
+                </label>
+              </label>
+            </>
+          ) : (
+            <>
+              <label className="input input-bordered flex items-center gap-2 mb-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="w-4 h-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder="Password"
+                  onChange={(ev) => {
+                    SetPassword(ev.target.value);
+                  }}
+                  value={password}
+                />
+                <label className="swap">
+                  <input type="checkbox" />
+                  <div className="swap-on">Hide</div>
+                  <div
+                    className="swap-off"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  >
+                    Show
+                  </div>
+                </label>
+              </label>
+            </>
+          )}
 
           <button className="btn btn-active btn-primary" onClick={userSignin}>
             Sign in
